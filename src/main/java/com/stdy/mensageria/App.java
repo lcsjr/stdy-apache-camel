6bfd9dff04254a0b1a0414522f273f2f24a7aac5
Luiz Santos <lcsjr.s23@gmail.com>
2019-07-02 01:22:04 -0300

    recebendo mensagem - fase inicial

---
+++ src/main/java/com/stdy/mensageria/App.java
@@ -0,0 +1,37 @@
+package com.stdy.mensageria;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class App {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+    
+		InitialContext context = new InitialContext();
+		
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory") ;
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination destination = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(destination);
+		
+		Message message = consumer.receive();
+		
+		System.out.println("Recebendo mensagem: "+ message);
+		
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
