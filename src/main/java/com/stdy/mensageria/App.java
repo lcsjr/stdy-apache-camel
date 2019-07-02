0c1c6113bdb37067f40bf11d88c7773781613129
Luiz Santos <lcsjr.s23@gmail.com>
2019-07-02 02:10:55 -0300

    capturando corpo mensagem

--- src/main/java/com/stdy/mensageria/App.java
+++ src/main/java/com/stdy/mensageria/App.java
@@ -1,17 +1,22 @@
 package com.stdy.mensageria;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class App {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws NamingException, JMSException {
     
 		InitialContext context = new InitialContext();
@@ -25,10 +30,27 @@ public class App {
 		Destination destination = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(destination);
 		
-		Message message = consumer.receive();
+//		Message message = consumer.receive();
+//		System.out.println("Recebendo apenas uma mensagem: "+ message);
 		
-		System.out.println("Recebendo mensagem: "+ message);
+		consumer.setMessageListener( new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				
+				TextMessage textMessage = (TextMessage) message; 
+				
+				try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
+		
+
+		new Scanner(System.in).nextLine();
 		
 		session.close();
 		connection.close();
